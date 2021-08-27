3faa23423c860102b060a04e60066f5677aa3ce2
Dev Spain <50495488+DmitrTRC@users.noreply.github.com>
2021-08-27 20:09:38 +0200

    Update las_1.py
    Review from DM

--- las_1.py
+++ las_1.py
@@ -1,3 +1,6 @@
+#TODO:  Measure only integers
+#TODO: Save measurements to data stracture and print as table
+# Refactor code
 n=int(input("Введите количество измерений: "))
 for i in range(1,n):
     a=int(input(""))
