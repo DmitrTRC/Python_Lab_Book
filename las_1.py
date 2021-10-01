b45a23eeb7fb1ce2e2db25e99aa78a23832111f2
Dmitry Morozov <9984398@gmail.com>
2021-10-01 19:20:48 +0200

    Refactored

--- las_1.py
+++ las_1.py
@@ -1,4 +1,17 @@
-n=int(input("Введите количество измерений: "))
-for i in range(1,n):
-    a=int(input(""))
-    print(i,"измерение: ",a)
+# n=int(input("Введите количество измерений: "))
+# for i in range(1,n):
+#     a=int(input(""))
+#     print(i,"измерение: ",a)
+
+
+measurements = []
+
+measurements_number = int(input("Введите количество измерений электрических сопротивлений: "))
+for i in range(measurements_number):
+    print("Введите измерение: ", end='')
+    measurements.append(float(input()))
+
+print()
+
+for index, measurement in enumerate(measurements, 1):
+    print(f'Measurement #{index} R = {measurement} ohms')
